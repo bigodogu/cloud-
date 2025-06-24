e66f768fb25b0252bc3f348ece60743cc441a941
vamshi4703 <vamshi4703@gmail.com>
2025-06-24 20:21:30 +0530

    Update github-api-integration-module.sh
    commited on ui

--- github-api-integration-module.sh
+++ github-api-integration-module.sh
@@ -3,7 +3,7 @@
 # Author: Abhishek
 # Version: v1
 #
-#git fetch
+#
 #
 i# This script will help users to communicate and retrieve information from GitHub
 # Usage:
@@ -13,6 +13,7 @@ i# This script will help users to communicate and retrieve information from GitH
 ################################
 forked repo
 second line
+git fetch
 if [ ${#@} -lt 2 ]; then
     echo "usage: $0 [your github token] [REST expression]"
     exit 1;
