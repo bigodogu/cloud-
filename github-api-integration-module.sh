ef20a0074670b71723a647bed64624bce061156b
Chandu-999 <95142741+Chandu-999@users.noreply.github.com>
2025-06-27 15:32:59 +0530

    Update github-api-integration-module.sh
    chandu updated the description as per exercise

--- github-api-integration-module.sh
+++ github-api-integration-module.sh
@@ -5,13 +5,14 @@
 #
 #
 #
+#
 # This script will help users to communicate and retrieve information from GitHub
 # Usage:
 #   Please provide your github token and rest api to the script as input
 #
 #
 ################################
-
+#chandu is updating the description
 if [ ${#@} -lt 2 ]; then
     echo "usage: $0 [your github token] [REST expression]"
     exit 1;
