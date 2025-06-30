bfff9861aa27dfb403dbd78505c1b6de202c5dcb
varshini357 <varshini8913@gmail.com>
2025-06-30 11:52:36 +0530

    Update github-api-integration-module.sh

--- github-api-integration-module.sh
+++ github-api-integration-module.sh
@@ -5,10 +5,10 @@
 #
 #
 #
-# This script will help users to communicate and retrieve information from GitHub
+# This script will help users to communicate and retrieve information from GitHubbb
 # Usage:
 #   Please provide your github token and rest api to the script as input
-#
+# 
 #
 ################################
 
