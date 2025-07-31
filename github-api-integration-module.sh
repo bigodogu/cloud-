1fa3a38eb29728838ee4ff3177b7e52353abc507
agrawalankit110 <86000686+agrawalankit110@users.noreply.github.com>
2025-07-31 12:09:48 +0530

    Update github-api-integration-module.sh

--- github-api-integration-module.sh
+++ github-api-integration-module.sh
@@ -2,9 +2,7 @@
 ################################
 # Author: Abhishek
 # Version: v1
-#
-#
-#
+
 # This script will help users to communicate and retrieve information from GitHub
 # Usage:
 #   Please provide your github token and rest api to the script as input
