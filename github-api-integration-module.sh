a73835742d95d33d23bc82c037ccd29f15a55e82
Ashitosh2031 <ashitoshmane31@gmail.com>
2026-02-04 00:53:31 +0530

    new cmt

--- github-api-integration-module.sh
+++ github-api-integration-module.sh
@@ -9,7 +9,7 @@
 # Usage:
 #   Please provide your github token and rest api to the script as input
 #   Ashitosh Mane is a DevOps Engg
-#
+#   Currently i am working as an RPA Developer
 ################################
 
 if [ ${#@} -lt 2 ]; then
